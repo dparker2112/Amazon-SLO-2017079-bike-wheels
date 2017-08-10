416c7d3818f58cc65fdf2ced2f96cfe3a07c27ee
david d parker <nlightn0@gmail.com>
2017-08-10 08:03:52 -0700

    Added Sin Function, interleaved fade

--- src/bike wheels.ino
+++ src/bike wheels.ino
@@ -6,7 +6,6 @@ ANIMATION IS SLOW AND SUBTLE.
 Connect opposite pairs of spokes on both wheels to defined PWM pins on Arduino
 */
 
-<<<<<<< HEAD
 int value, value2, value3, value4 ;
 int ledpin = 3;                           // light connected to digital pin 3
 int ledpin2 = 5;                          // light connected to digital pin 5
@@ -33,56 +32,4 @@ void loop()
  analogWrite(ledpin2, value2);         // sets the value (range from 0 to 255)
  analogWrite(ledpin3, value3);         // sets the value (range from 0 to 255)
  analogWrite(ledpin4, value4);         // sets the value (range from 0 to 255)
-=======
-int Aspoke = 3;
-int Bspoke = 5;
-int Cspoke = 6;
-int Dspoke = 9;
-
-int brightness = 0;    // how bright the LED is
-int fadeAmount = 1;    // how many points to fade the LED by
-
-void setup() {
-  pinMode(Aspoke, OUTPUT);
-  pinMode(Bspoke, OUTPUT);
-  pinMode(Cspoke, OUTPUT);
-  pinMode(Dspoke, OUTPUT);
-}
-
-
-void loop() {
-  // set the brightness of pin 11:
-  analogWrite(Aspoke, brightness);
-
-  // change the brightness for next time through the loop:
-  brightness = brightness + fadeAmount;
-
-  // reverse the direction of the fading at the ends of the fade:
-  if (brightness <= 0 || brightness >= 255) {
-    fadeAmount = -fadeAmount;
-  }
-  // wait for 30 milliseconds to see the dimming effect
-  delay(4);
-
-  analogWrite(Bspoke, brightness);
-  brightness = brightness + fadeAmount;
-  if (brightness <= 0 || brightness >= 255) {
-    fadeAmount = -fadeAmount;
-  }
-  delay(4);
-
-  analogWrite(Cspoke, brightness);
-  brightness = brightness + fadeAmount;
-  if (brightness <= 0 || brightness >= 255) {
-    fadeAmount = -fadeAmount;
-  }
-  delay(4);
-
-  analogWrite(Dspoke, brightness);
-  brightness = brightness + fadeAmount;
-  if (brightness <= 0 || brightness >= 255) {
-    fadeAmount = -fadeAmount;
-  }
-  delay(4);
->>>>>>> origin/master
 }
